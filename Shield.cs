5ba55cbc5c3adcfac4baa2eb027598e8a5138f48
Sakura <2724765372@qq.com>
2026-05-07 02:23:25 +0800

    Delete Shield.cs

--- Shield.cs
+++
@@ -1,3 +0,0 @@
-// MR OSD Shield source has been split into src/.
-// This file is intentionally kept as a migration note for older releases.
-// Build with compile.bat, which compiles all .cs files under src/.
